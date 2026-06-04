c1387640f705d07d923dbe799a9ec92106173551
Eduardo Tribst <eduardo.tribst@gmail.com>
2026-06-04 13:48:26 -0300

    geração de mapas feita

--- ProjJogosDigitais/Assets/Scripts/MapGenerator.cs
+++ ProjJogosDigitais/Assets/Scripts/MapGenerator.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [Header("Map Layouts")]
+    [Header("Formatos de Mapas (Layout Prefabs)")]
     public GameObject[] mapLayouts; 
+
+    [Header("Estilos de Plataforma")]
+    public Sprite[] themeSprites; 
 
     void Awake()
     {
@@ -14,11 +17,28 @@ public class MapGenerator : MonoBehaviour
     {
         if (mapLayouts.Length == 0)
         {
+            Debug.LogWarning("Nenhum layout atribuído ao Map Generator!");
             return;
         }
 
-        int randomIndex = Random.Range(0, mapLayouts.Length);
+        int randomLayoutIndex = Random.Range(0, mapLayouts.Length);
+        GameObject spawnedLayout = Instantiate(mapLayouts[randomLayoutIndex], Vector3.zero, Quaternion.identity);
 
-        Instantiate(mapLayouts[randomIndex], Vector3.zero, Quaternion.identity);
+        if (themeSprites.Length > 0)
+        {
+            int randomSpriteIndex = Random.Range(0, themeSprites.Length);
+            Sprite chosenTheme = themeSprites[randomSpriteIndex];
+
+            SpriteRenderer[] platformRenderers = spawnedLayout.GetComponentsInChildren<SpriteRenderer>();
+
+            foreach (SpriteRenderer renderer in platformRenderers)
+            {
+                renderer.sprite = chosenTheme;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Nenhum sprite de tema foi colocado na lista do Map Generator!");
+        }
     }
 }
