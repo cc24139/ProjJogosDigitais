e7f5af0c4a735143174e5430d423a4845e7691f5
Eduardo Tribst <eduardo.tribst@gmail.com>
2026-06-16 18:13:34 -0300

    health bars bonitas e funcionando

--- ProjJogosDigitais/Assets/Scripts/GameSpawner.cs
+++ ProjJogosDigitais/Assets/Scripts/GameSpawner.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSpawner : MonoBehaviour
 {
     [Header("Character Prefabs")]
-    public GameObject[] characterPrefabs; 
+    public GameObject[] characterPrefabs;
 
+    [Header("Referências da UI")]
+    public Image healthBarFillP1; 
+    public Image healthBarFillP2; 
 
     void Start()
     {
@@ -16,26 +20,19 @@ public class GameSpawner : MonoBehaviour
         GameObject p1Point = GameObject.Find("P1_SpawnPoint");
         GameObject p2Point = GameObject.Find("P2_SpawnPoint");
 
-        if (p1Point == null || p2Point == null)
-        {
-            Debug.LogError("ERRO: Não encontrei os objetos 'P1_SpawnPoint' ou 'P2_SpawnPoint' no mapa gerado!");
-            return;
-        }
 
         int p1ID = Menu.player1SelectedID;
         int p2ID = Menu.player2SelectedID;
 
-        GameObject p1Prefab = characterPrefabs[p1ID];
-        GameObject p1 = Instantiate(p1Prefab, p1Point.transform.position, p1Point.transform.rotation);
-        p1.name = "Player 1"; 
+        GameObject p1 = Instantiate(characterPrefabs[p1ID], p1Point.transform.position, p1Point.transform.rotation);
+        p1.name = "Player 1";
         p1.GetComponent<WalkingCharacter>().playerID = 1;
+        p1.GetComponent<PlayerHealth>().healthBarFill = healthBarFillP1;
 
-        GameObject p2Prefab = characterPrefabs[p2ID];
-        GameObject p2 = Instantiate(p2Prefab, p2Point.transform.position, p2Point.transform.rotation);
+        GameObject p2 = Instantiate(characterPrefabs[p2ID], p2Point.transform.position, p2Point.transform.rotation);
         p2.name = "Player 2";
         p2.transform.localScale = new Vector3(-p2.transform.localScale.x, p2.transform.localScale.y, p2.transform.localScale.z);
         p2.GetComponent<WalkingCharacter>().playerID = 2;
-        
-        p2.transform.localScale = new Vector3(-p2.transform.localScale.x, p2.transform.localScale.y, p2.transform.localScale.z);
+        p2.GetComponent<PlayerHealth>().healthBarFill = healthBarFillP2;
     }
 }
--- ProjJogosDigitais/Assets/Scripts/PlayerHealth.cs
+++ ProjJogosDigitais/Assets/Scripts/PlayerHealth.cs
@@ -3,49 +3,33 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [Header("Configurações de Vida")]
-    public float maxHealth = 100f; 
+    public float maxHealth = 100f;
     private float currentHealth;
 
-    private Image healthBar;
+    public Image healthBarFill; 
 
     void Start()
     {
         currentHealth = maxHealth;
-
-        WalkingCharacter pc = GetComponent<WalkingCharacter>();
-        if (pc != null)
-        {
-            if (pc.playerID == 1)
-            {
-                healthBar = GameObject.Find("HealthBar_P1")?.GetComponent<Image>();
-            }
-            else
-            {
-                healthBar = GameObject.Find("HealthBar_P2")?.GetComponent<Image>();
-            }
-        }
+        UpdateVisuals();
     }
 
     public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0f, maxHealth);
+        UpdateVisuals();
 
-        if (healthBar == null)
-        {
-            int id = GetComponent<WalkingCharacter>().playerID;
-            healthBar = GameObject.Find($"HealthBar_P{id}")?.GetComponent<Image>();
-        }
-
-        if (healthBar != null)
-        {
-            healthBar.fillAmount = currentHealth / maxHealth;
-        }
-        
         if (currentHealth <= 0)
         {
             Die();
+        }
+    }
+
+    private void UpdateVisuals()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = currentHealth / maxHealth;
         }
     }
 
